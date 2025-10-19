0046e11f5128469881b4ba7c0cce71350b5c3063
_C∅FFiN_ <62094188+C0FFiN-prod@users.noreply.github.com>
2025-10-20 01:43:42 +0300

    Update drawing.ts

--- src/drawing.ts
+++ src/drawing.ts
@@ -1,4 +1,4 @@
-import { gateModeToType, gateTypeToMode, pathMap, ToolMode } from "./consts";
+import { gateModeToType, gateTypeToMode, pathMap, ShowWiresMode, ToolMode } from "./consts";
 import { LogicElement, LogicGate, isInputElement, isOutputElement } from "./logic";
 import {
     camera, canvas, gridSize,
@@ -6,8 +6,10 @@ import {
     isSelecting, selectionEnd, selectionStart,
     selectedElements, selectedSources, selectedTargets,
     selectedTool, circuit, elementUnderCursor,
-    type Point
+    type Point,
+    showWiresMode
 } from "./main";
+import { hexToRgb, luminance, lightness, rgbToHex } from "./utils";
 
 let ctx: CanvasRenderingContext2D;
 
@@ -51,68 +53,40 @@ export function draw() {
 
     drawGrid();
 
-    if (selectedTool === ToolMode.Connect) {
-        // Draw wires
-        ctx.strokeStyle = '#888';
-        ctx.lineWidth = 1;
-        ctx.beginPath();
-
-        for (const wire of circuit.wires.values()) {
-            let start = worldToTranslatedScreen(wire.src.x, wire.src.y);
-            let end = worldToTranslatedScreen(wire.dst.x, wire.dst.y);
-            ctx.moveTo(start.x + gridSize, start.y + gridSize * .5);
-            ctx.lineTo(end.x, end.y + gridSize * .5);
-
-        }
-        ctx.stroke();
-
-        // Draw temporary connections between selected sources and targets
-        if (selectedSources.size > 0 && selectedTargets.size > 0) {
-            ctx.strokeStyle = '#fa0';
-            ctx.lineWidth = 1;
-            ctx.beginPath();
-            for (const source of selectedSources) {
-                for (const target of selectedTargets) {
-                    let start = worldToTranslatedScreen(source.x, source.y);
-                    let end = worldToTranslatedScreen(target.x, target.y);
-                    ctx.moveTo(start.x + gridSize, start.y + gridSize * .5);
-                    ctx.lineTo(end.x, end.y + gridSize * .5);
-                }
-            }
-            ctx.stroke();
-        }
-    }
+    drawWires();
 
     // Draw elements
     for (const el of circuit.elements) {
         drawElement(el);
-    }
-
-    // Highlight selected sources (green)
-    ctx.lineWidth = 3;
-    for (const el of selectedSources) {
-        drawBorder(el, '#0f0');
-    }
-
-    // Highlight selected targets (red)
-    for (const el of selectedTargets) {
-        drawBorder(el, '#f00');
-    }
-
-    // Highlight selected elements (blue)
-    for (const el of selectedElements) {
-        drawBorder(el, '#19f');
+        let borderColor = "#555";
+        if (selectedSources.has(el) && selectedTargets.has(el))
+            borderColor = '#ff0';
+        else if (selectedSources.has(el))
+            borderColor = '#0f0';
+        else if (selectedTargets.has(el))
+            borderColor = '#f00';
+        else if (selectedElements.has(el))
+            borderColor = '#19f';
+        drawBorder(el, borderColor);
     }
 
     if (elementUnderCursor) {
         for (const el of elementUnderCursor.inputs) {
+            if (el === elementUnderCursor) continue;
             const { x, y } = worldToTranslatedScreen(el.x, el.y);
-            writeTextAt(x + gridSize * 0.15, y + gridSize * 0.75, gridSize * 0.7, "#0f0", "IN");
+            writeTextAt(x + gridSize * 0.55, y + gridSize * 0.55, gridSize * 0.5, "#0f0", "IN");
         }
         for (const el of elementUnderCursor.outputs) {
+            if (el === elementUnderCursor) continue;
             const { x, y } = worldToTranslatedScreen(el.x, el.y);
-            writeTextAt(x - gridSize * 0.2, y + gridSize * 0.75, gridSize * 0.7, "#f00", "OUT");
+            writeTextAt(x + gridSize * 0.55, y + gridSize * 0.55, gridSize * 0.5, "#f00", "OUT");
         }
+        const { x, y } = worldToTranslatedScreen(elementUnderCursor.x, elementUnderCursor.y);
+        if (elementUnderCursor.inputs.has(elementUnderCursor))
+            writeTextAt(x + gridSize * 0.55, y + gridSize * 0.55, gridSize * 0.5, "#ff0", "SW");
+        else
+            writeTextAt(x + gridSize * 0.55, y + gridSize * 0.55, gridSize * 0.5, "#19f", "X");
+
     }
 
     ctx.setTransform(1, 0, 0, 1, 0, 0);
@@ -132,14 +106,55 @@ export function draw() {
 
 }
 
+function drawWires() {
+    if (showWiresMode === ShowWiresMode.Always ||
+        showWiresMode === ShowWiresMode.Connect && selectedTool === ToolMode.Connect) {
+        // Draw wires
+        ctx.strokeStyle = '#888';
+        ctx.lineWidth = 1 / camera.zoom;
+        ctx.beginPath();
+
+        for (const wire of circuit.wires.values()) {
+            let start = worldToTranslatedScreen(wire.src.x, wire.src.y);
+            let end = worldToTranslatedScreen(wire.dst.x, wire.dst.y);
+            ctx.moveTo(start.x + gridSize, start.y + gridSize * .5);
+            ctx.lineTo(end.x, end.y + gridSize * .5);
+
+        }
+        ctx.stroke();
+    }
+
+    // Draw temporary connections between selected sources and targets
+    if (
+        (
+            showWiresMode === ShowWiresMode.Always ||
+            (showWiresMode === ShowWiresMode.Connect ||
+                showWiresMode === ShowWiresMode.Temporary) && selectedTool === ToolMode.Connect
+        ) &&
+        selectedSources.size > 0 && selectedTargets.size > 0) {
+        ctx.strokeStyle = '#fa0';
+        ctx.lineWidth = 1 / camera.zoom;
+        ctx.beginPath();
+        for (const source of selectedSources) {
+            for (const target of selectedTargets) {
+                let start = worldToTranslatedScreen(source.x, source.y);
+                let end = worldToTranslatedScreen(target.x, target.y);
+                ctx.moveTo(start.x + gridSize, start.y + gridSize * .5);
+                ctx.lineTo(end.x, end.y + gridSize * .5);
+            }
+        }
+        ctx.stroke();
+    }
+}
+
 function drawBorder(el: LogicElement, color: string | CanvasGradient | CanvasPattern) {
     ctx.strokeStyle = color;
-    ctx.lineWidth = 2;
+    ctx.lineWidth = 1/camera.zoom;
     ctx.strokeRect(
-        el.x * gridSize - 2,
-        el.y * gridSize - 2,
-        gridSize + 4,
-        gridSize + 4
+        el.x * gridSize,
+        el.y * gridSize,
+        gridSize,
+        gridSize
     );
 }
 
@@ -153,12 +168,21 @@ function drawElement(el: LogicElement) {
     //     (0 < end_y && end_y < canvas.height)
     // ) {
     const wx = el.x * gridSize;
-    const wy = el.y * gridSize;
+    const wy = el.y * gridSize; 
+    
+    const [r, g, b] = hexToRgb(el.color);
+    let isLuminant = luminance(r, g, b) >= 0.5;
+    let isBright = lightness(r, g, b) >= 0.5;
+    const shiftedColor = rgbToHex(
+        Math.floor(r * 0.5 + 63.75),
+        Math.floor(g * 0.5 + 63.75),
+        Math.floor(b * 0.5 + 63.75)
+    );
     ctx.save();
     ctx.translate(wx, wy);
     ctx.strokeStyle = '#555';
-    ctx.fillStyle = el.value ? '#15c' : el.color;
-    ctx.lineWidth = 2 / camera.zoom;
+    ctx.fillStyle = (el.value === isBright) ? el.color : shiftedColor;
+    ctx.lineWidth = 1 / camera.zoom;
     ctx.beginPath();
     ctx.rect(0, 0, gridSize, gridSize);
     ctx.fill();
@@ -179,33 +203,8 @@ function drawElement(el: LogicElement) {
     if (camera.zoom > 0.65) {
         let path = pathMap.get((el.type == 'GATE' ? gateModeToType.get((el as LogicGate).gateType)! : el.type).toLowerCase());
         if (path)
-            drawSvgSymbol(path, 5, 5, 10 / 22, (el.value ? '#59f' : '#eee'), 1);
+            drawSvgSymbol(new Path2D(path), 5, 5, 10 / 22, (isLuminant ? rgbToHex(r - 127, g - 127, b - 127) : rgbToHex(r + 127, g + 127, b + 127)), 1);
     }
-
-    if (selectedTool === ToolMode.Connect) {
-        // Вход
-        if (!isInputElement(el)) {
-            ctx.beginPath();
-            ctx.arc(0, gridSize * .5, 2, 0, 2 * Math.PI);
-            ctx.fillStyle = el.value ? '#0a0' : '#aaa';
-            ctx.fill();
-            ctx.stroke();
-        }
-
-        // Выход
-        if (!isOutputElement(el)) {
-            ctx.beginPath();
-            ctx.arc(gridSize, gridSize * .5, 2, 0, 2 * Math.PI);
-            ctx.fillStyle = el.value ? '#0a0' : '#aaa';
-            ctx.fill();
-            ctx.stroke();
-        }
-    }
-
-
-
-
-
     ctx.restore();
     // }
 
@@ -252,6 +251,12 @@ function writeTextAt(x: number, y: number, fontSize: number, color: string, ...d
     ctx.save();
     ctx.fillStyle = color;
     ctx.font = `${fontSize}px sans-serif`;
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'middle';
+    ctx.shadowColor = "black";   // цвет тени
+    ctx.shadowBlur = 4;          // размытие
+    ctx.shadowOffsetX = 0;       // смещение по X
+    ctx.shadowOffsetY = 0;  // смещение по Y
     let str = "";
     for (const d of data) {
         str += d + ' ';
